02ffacc876f093261094adad84cf8a0e7846d405
Yanu <59870900+khusanova@users.noreply.github.com>
2026-03-22 01:05:17 +0100

    Include age into the filter option selection

--- filter_users.py
+++ filter_users.py
@@ -39,8 +39,8 @@ def filter_users_by_name(name: str):
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, "
-                          "only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, only "
+                          "'name' and 'age' are supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
